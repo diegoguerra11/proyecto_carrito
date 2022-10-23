9718255b397277596dc9b5920e24931a77bc42be
PlundereR0412 <dereckmunoz07@gmail.com>
2022-10-23 18:02:50 -0500

    new validationsIniciarSesio y RegistroRapido

--- tienda/src/app/components/login/login.component.ts
+++ tienda/src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ClienteService } from 'src/app/services/cliente.service';
 import { ValidatonsCliente } from 'src/app/validations/validationsCliente';
+import { ValidatonsIniciarSesion } from 'src/app/validations/validationsIniciarSesion';
 import { MessageBox } from '../../Utils/MessageBox';
 
 @Component({
@@ -11,7 +12,7 @@ import { MessageBox } from '../../Utils/MessageBox';
 })
 
 export class LoginComponent implements OnInit {
- 
+
   public user : any = {};
   public usuario : any = {};
   public token;
@@ -20,37 +21,38 @@ export class LoginComponent implements OnInit {
   constructor(
     private _clienteService: ClienteService,
     private _router : Router
-  ) { 
+  ) {
     this.token = localStorage.getItem('token');
-    
+
     if(this.token){this._router.navigate(['/']);}
   }
 
   ngOnInit(): void {}
 
   login(loginForm:any) {
-    if(!loginForm.valid) {MessageBox.messageError('Los datos del formulario no son validos'); return;}
+    if(!ValidatonsIniciarSesion.login(loginForm.form.value)){return;}
+    console.log(this.login);
 
     let data = {
       email: this.user.email,
       password: this.user.password
     }
-    
+
     let productos = localStorage['cart'] ? JSON.parse(localStorage['cart']) : undefined;
 
     this._clienteService.login_cliente(data).subscribe(
-      response=>{ 
+      response=>{
         if(response.data == undefined){MessageBox.messageError(response.message); return;}
 
         this.usuario = response.data;
 
         localStorage.setItem('token', response.token);
         localStorage.setItem('_id', response.data._id);
-        
+
         if(productos){
           this.registrarProductos(
-            localStorage.getItem('_id'), 
-            productos, 
+            localStorage.getItem('_id'),
+            productos,
             localStorage.getItem('token')
           );
         }
@@ -68,18 +70,18 @@ export class LoginComponent implements OnInit {
         cantidad: producto.cantidad,
         variedad: producto.variedad.variedad,
       }
-      
+
       this._clienteService.agregar_carrito_cliente(data, token).subscribe(
         response=>{},
         error=>{console.log(error);}
-      )  
+      )
     })
   }
 
   registrar(loginForm:any) {
-    if(!loginForm.valid) {MessageBox.messageError('Debe completar todos los campos'); return;}
+
     if(!ValidatonsCliente.registrarCliente(loginForm.form.value)){return;}
-    
+
     this._clienteService.registro_cliente(this.cliente).subscribe(
       response =>{
         if(response.data == undefined) {MessageBox.messageError("Cuenta existente"); return;}
@@ -88,11 +90,11 @@ export class LoginComponent implements OnInit {
           email: this.cliente.email,
           password: this.cliente.password
         }
-        
+
         this._clienteService.login_cliente(data).subscribe(
-          response=>{ 
+          response=>{
             if(response.data == undefined){MessageBox.messageError(response.message); return;}
-    
+
             this.usuario = response.data;
             localStorage.setItem('token', response.token);
             localStorage.setItem('_id', response.data._id);
--- tienda/src/app/validations/validationsCliente.ts
+++ tienda/src/app/validations/validationsCliente.ts
@@ -1,9 +1,9 @@
 import { MessageBox } from '../Utils/MessageBox';
 
 export class ValidatonsCliente {
-    static actualizarCliente(form: any) {      
+    static actualizarCliente(form: any) {
         let numerico = /^[0-9]+$/;
-        
+
         if(!form.nombres) {
             MessageBox.messageError('El campo nombres es obligatorio');
             return false;
@@ -13,7 +13,7 @@ export class ValidatonsCliente {
             MessageBox.messageError('El campo nombres debe ser alfabetico');
             return false;
         }
-        
+
         if(form.telefono && !form.telefono.match(numerico)) {
             MessageBox.messageError('El campo telefono debe ser numerico');
             return false;
@@ -26,17 +26,33 @@ export class ValidatonsCliente {
         let esCorreo = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
         if(!form.nombres){
-            MessageBox.messageError('El campo nombres es obligatorio');
+            MessageBox.messageError('El campo nombre es obligatorio');
             return false;
         }
         if(form.nombres.match(numerico)) {
-            MessageBox.messageError('El campo nombres debe ser alfabetico');
+            MessageBox.messageError('El campo nombre debe ser alfabetico');
             return false;
+        }
+        if(!form.apellidos){
+          MessageBox.messageError('El campo apellido es obligatorio');
+          return false;
+        }
+      if(form.apellidos.match(numerico)) {
+          MessageBox.messageError('El campo apellido debe ser alfabetico');
+          return false;
+        }
+      if(!form.email){
+        MessageBox.messageError('El campo email es obligatorio');
+        return false;
         }
         if(!form.email.match(esCorreo)) {
             MessageBox.messageError('Debe Ingresar un correo electrónico válido');
             return false;
         }
+        if(!form.password){
+          MessageBox.messageError('El campo password es obligatorio');
+          return false;
+          }
 
         return true;
 
