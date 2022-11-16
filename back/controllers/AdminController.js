6a3cef1e4de9d875005f6c89678df2c47d749e63
RenzoCM <renzo_Dan_@hotmail.com>
2022-11-16 02:04:34 -0500

    Dashboard y KPIS

--- back/controllers/AdminController.js
+++ back/controllers/AdminController.js
@@ -344,13 +344,122 @@ const enviar_email = async function(venta, motivo) {
         });
     }); 
 }
+//KPIS 
+const kpi_ganancias_mensuales_admin  = async function(req,res){
 
+    //Restricción realizada a los usuarios: si no son usuarios con el rol de administrador, no podrán acceder a esta función.
+    if(!req.user || req.user.role != 'admin') {return res.status(500).send({message: 'NoAccess'});}
+    
+   let enero = 0;
+   let febrero = 0;
+   let marzo = 0;
+   let abril = 0;
+   let mayo = 0;
+   let junio = 0;
+   let julio = 0;
+   let agosto = 0;
+   let septiembre = 0;
+   let octubre = 0;
+   let noviembre = 0;
+   let diciembre = 0;
+    
+   let total_ganancia = 0;
+   let total_mes = 0;
+   let cont_ventas = 0;
+   let total_mes_anterior = 0;
+   
+    let buscar_ventas = Promise.resolve(Venta.find());
+    let current_date = new Date();
+    let current_year = current_date.getFullYear();
+    let current_month = current_date.getMonth()+1;
+
+    buscar_ventas.then(ventas => {
+
+        for(let venta of ventas){
+            cont_ventas++;
+            let createdAt_date = new Date(venta.createdAt);
+            let mes = createdAt_date.getMonth()+1;
+            if(createdAt_date.getFullYear() == current_year){
+            total_ganancia =total_ganancia+ venta.subtotal;
+            if(mes == current_month){
+                total_mes = total_mes + venta.subtotal;
+            }
+            if(mes == current_month-1){
+                total_mes_anterior = total_mes_anterior + venta.subtotal;
+            }
+            switch (mes) {
+                case 1:
+                    enero = enero + venta.subtotal;
+                    break;
+                case 2:
+                    febrero = febrero + venta.subtotal;
+                    break;
+                case 3:
+                    marzo = marzo + venta.subtotal;
+                    break;  
+                case 4:
+                    abril = abril + venta.subtotal;
+                    break;
+                case 5:
+                    mayo = mayo + venta.subtotal;
+                    break;
+                case 6:
+                    junio =  junio + venta.subtotal;
+                    break;   
+                case 7:
+                    julio = julio + venta.subtotal;
+                    break;
+                case 8:
+                    agosto = agosto + venta.subtotal;
+                    break;
+                case 9:
+                    septiembre = septiembre + venta.subtotal;
+                    break;  
+                case 10:
+                    octubre = octubre + venta.subtotal;
+                    break;
+                case 11:
+                    noviembre = noviembre + venta.subtotal;
+                    break;
+                case 12:
+                    diciembre = diciembre + venta.subtotal;
+                    break; 
+                
+            }
+        }
+            
+        }
+
+        
+        res.status(200).send({
+            enero:enero, 
+            febrero: febrero,
+             marzo:marzo,
+            abril:abril,
+            mayo:mayo,
+            junio:junio,
+            julio:julio,
+            agosto:agosto,
+            septiembre:septiembre,
+            octubre:octubre,
+            noviembre:noviembre,
+            diciembre:diciembre,
+            total_ganancia:total_ganancia,
+            total_mes:total_mes,
+            cont_ventas:cont_ventas,
+            total_mes_anterior:total_mes_anterior,
+
+        
+        });
+    });
+}
 //Exportación de las funciones.
 module.exports ={
     registro_admin,
     login_admin,
     obtener_ventas_admin,
     listar_variedades_admin,
+    kpi_ganancias_mensuales_admin,
     listar_variedades_productos_admin,
     confirmar_pago_orden,
     obtener_detalles_ordenes_cliente,
